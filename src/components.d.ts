b69b5e392c33d7254ea427372aea3105ac2e6d56
Louis Bompart <lbompart@coveo.com>
2023-12-01 00:36:02 -0500

    add mirage

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,58 +6,58 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-  interface ResultsManager {}
-  interface SampleComponent {}
-  interface SampleResultComponent {}
+    interface ResultsManager {
+    }
+    interface SampleComponent {
+    }
+    interface SampleResultComponent {
+    }
 }
 declare global {
-  interface HTMLResultsManagerElement
-    extends Components.ResultsManager,
-      HTMLStencilElement {}
-  var HTMLResultsManagerElement: {
-    prototype: HTMLResultsManagerElement;
-    new (): HTMLResultsManagerElement;
-  };
-  interface HTMLSampleComponentElement
-    extends Components.SampleComponent,
-      HTMLStencilElement {}
-  var HTMLSampleComponentElement: {
-    prototype: HTMLSampleComponentElement;
-    new (): HTMLSampleComponentElement;
-  };
-  interface HTMLSampleResultComponentElement
-    extends Components.SampleResultComponent,
-      HTMLStencilElement {}
-  var HTMLSampleResultComponentElement: {
-    prototype: HTMLSampleResultComponentElement;
-    new (): HTMLSampleResultComponentElement;
-  };
-  interface HTMLElementTagNameMap {
-    "results-manager": HTMLResultsManagerElement;
-    "sample-component": HTMLSampleComponentElement;
-    "sample-result-component": HTMLSampleResultComponentElement;
-  }
+    interface HTMLResultsManagerElement extends Components.ResultsManager, HTMLStencilElement {
+    }
+    var HTMLResultsManagerElement: {
+        prototype: HTMLResultsManagerElement;
+        new (): HTMLResultsManagerElement;
+    };
+    interface HTMLSampleComponentElement extends Components.SampleComponent, HTMLStencilElement {
+    }
+    var HTMLSampleComponentElement: {
+        prototype: HTMLSampleComponentElement;
+        new (): HTMLSampleComponentElement;
+    };
+    interface HTMLSampleResultComponentElement extends Components.SampleResultComponent, HTMLStencilElement {
+    }
+    var HTMLSampleResultComponentElement: {
+        prototype: HTMLSampleResultComponentElement;
+        new (): HTMLSampleResultComponentElement;
+    };
+    interface HTMLElementTagNameMap {
+        "results-manager": HTMLResultsManagerElement;
+        "sample-component": HTMLSampleComponentElement;
+        "sample-result-component": HTMLSampleResultComponentElement;
+    }
 }
 declare namespace LocalJSX {
-  interface ResultsManager {}
-  interface SampleComponent {}
-  interface SampleResultComponent {}
-  interface IntrinsicElements {
-    "results-manager": ResultsManager;
-    "sample-component": SampleComponent;
-    "sample-result-component": SampleResultComponent;
-  }
+    interface ResultsManager {
+    }
+    interface SampleComponent {
+    }
+    interface SampleResultComponent {
+    }
+    interface IntrinsicElements {
+        "results-manager": ResultsManager;
+        "sample-component": SampleComponent;
+        "sample-result-component": SampleResultComponent;
+    }
 }
 export { LocalJSX as JSX };
 declare module "@stencil/core" {
-  export namespace JSX {
-    interface IntrinsicElements {
-      "results-manager": LocalJSX.ResultsManager &
-        JSXBase.HTMLAttributes<HTMLResultsManagerElement>;
-      "sample-component": LocalJSX.SampleComponent &
-        JSXBase.HTMLAttributes<HTMLSampleComponentElement>;
-      "sample-result-component": LocalJSX.SampleResultComponent &
-        JSXBase.HTMLAttributes<HTMLSampleResultComponentElement>;
+    export namespace JSX {
+        interface IntrinsicElements {
+            "results-manager": LocalJSX.ResultsManager & JSXBase.HTMLAttributes<HTMLResultsManagerElement>;
+            "sample-component": LocalJSX.SampleComponent & JSXBase.HTMLAttributes<HTMLSampleComponentElement>;
+            "sample-result-component": LocalJSX.SampleResultComponent & JSXBase.HTMLAttributes<HTMLSampleResultComponentElement>;
+        }
     }
-  }
 }
